80a65e6ae3a73a94715e86a0292a7e6b335e562d
Анастасия <89359100+Asenka90@users.noreply.github.com>
2023-03-11 08:24:11 +0400

    Add files via upload

---
+++ Task1b.py
@@ -0,0 +1,11 @@
+import numpy as np
+from math import factorial
+
+print("Из колоды в 52 карты извлекаются случайным образом 4 карты. б) Найти вероятность, что среди 4-х карт окажется хотя бы один туз.")
+def probability_ace(n, k, n1, k1):
+    return 1- (np.math.factorial(n) // (np.math.factorial(k) * np.math.factorial(n - k))) \
+        / (np.math.factorial(n1) // (np.math.factorial(k1) * np.math.factorial(n1 - k1))) 
+
+result = probability_ace(48, 4, 52, 4) 
+
+print(f"Probability ace: {result:.2f}")
---
+++ Task2.py
@@ -0,0 +1,13 @@
+import numpy as np
+from math import factorial
+
+print("На входной двери подъезда установлен кодовый замок, содержащий десять кнопок\
+    с цифрами от 0 до 9. Код содержит три цифры, которые нужно нажать одновременно.\
+        Какова вероятность того, что человек, не знающий код, откроет дверь с первой попытки?")
+p = 1 
+
+def probability_open(n, k):
+    return  1 / (np.math.factorial(n) // (np.math.factorial(k) * np.math.factorial(n - k)))
+
+result = probability_open(10, 3)
+print(f"Probability: {result:.3f}")
